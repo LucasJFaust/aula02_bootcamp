470a673e1eb028b04a4e49b64ef9b8f93769453e
LucasjFaust <lucasjfm.eng@gmail.com>
2024-03-31 19:09:49 -0300

    Adicionado exercicio 01

--- exercicios.py
+++ exercicios.py
@@ -1,19 +1,28 @@
 # #### Inteiros (`int`)
 
 # 1. Escreva um programa que soma dois números inteiros inseridos pelo usuário.
+
+numero_01 = int(input("Digite um número inteiro: "))
+numero_02 = int(input("Digite novamente um número inteiro: "))
+soma_numeros = numero_01 + numero_02
+print(f"A soma dos números inteiros é igual a: {soma_numeros}")
+
 # 2. Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
+
+
+
 # 3. Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
 # 4. Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
 
-try:
-    numero_01 = int(input("Inserir um número inteiro: "))
-    numero_02 = int(input("Insira o outro número: "))
-    resultado = numero_01 // numero_02
-    print(resultado)
-except ZeroDivisionError:
-    print("integer division or modulo by zero")
-except KeyboardInterrupt:
-    print("Acho que você não quis inserir um numero")
+# try:
+#     numero_01 = int(input("Inserir um número inteiro: "))
+#     numero_02 = int(input("Insira o outro número: "))
+#     resultado = numero_01 // numero_02
+#     print(resultado)
+# except ZeroDivisionError:
+#     print("integer division or modulo by zero")
+# except KeyboardInterrupt:
+#     print("Acho que você não quis inserir um numero")
 
 # 5. Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.
 
